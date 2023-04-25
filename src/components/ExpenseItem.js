a85376856eed3fc6166f4fc0290f4fa3d1490046
kirushanthi26 <kirushanthikiru26@gmail.com>
2023-04-25 10:57:09 +0530

    add props to expenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,17 @@
-import React from 'react'
-import "./ExpenseItem.css"
+import React from "react";
+import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-
-  const expenseDate = new Date(2022, 2, 25)
-  const expenseTitle = 'car insurence';
-  const expenseAmount = 258.33;
+const ExpenseItem = (props) => {
 
   return (
-    <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
-      <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+    <div className="expense-item">
+      <div>{props.dateProps.toString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.titleProps}</h2>
+        <div className="expense-item__price">${props.amountProps}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ExpenseItem
+export default ExpenseItem;
