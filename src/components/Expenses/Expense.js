6bd33f5151ad053f2eb985aa790aabf422f94cdf
kirushanthi26 <kirushanthikiru26@gmail.com>
2023-05-04 10:02:12 +0530

    added filter

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ExpenseItem from './ExpenseItem'
 import './Expense.css'
 import Card from '../UI/Card'
+import ExpensesFilter from '../Filter/ExpensesFilter'
 
 const Expense = (props) => {
+  const [filteredYear, setFilterYear] = useState("2020");
+
+  const onChangeFilter = (selectedYear) => {
+    setFilterYear(selectedYear)
+  }
+
   return (
     <Card className='expenses'>
+
+    <ExpensesFilter onChangeFilter={onChangeFilter} selected={filteredYear}/>
+
       <ExpenseItem
         titleProps={props.expensesDummyData[0].title}
         amountProps={props.expensesDummyData[0].amount}
@@ -15,6 +25,16 @@ const Expense = (props) => {
         titleProps={props.expensesDummyData[1].title}
         amountProps={props.expensesDummyData[1].amount}
         dateProps={props.expensesDummyData[1].date}
+      />
+            <ExpenseItem
+        titleProps={props.expensesDummyData[2].title}
+        amountProps={props.expensesDummyData[2].amount}
+        dateProps={props.expensesDummyData[2].date}
+      />
+      <ExpenseItem
+        titleProps={props.expensesDummyData[3].title}
+        amountProps={props.expensesDummyData[3].amount}
+        dateProps={props.expensesDummyData[3].date}
       />
     </Card>
   )
