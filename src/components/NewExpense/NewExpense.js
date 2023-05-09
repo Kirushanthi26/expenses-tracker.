c68659c43bb66e3d7d1fc831a2367cee5da6f30c
kirushanthi26 <kirushanthikiru26@gmail.com>
2023-05-09 15:45:13 +0530

    conditional content using usestate

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
 
 const NewExpense = (props) => {
+
+  const [isEditing, setIsEditing] = useState(false);
 
   const onSaveExpenseData = (enteredExpenseData) => {
     const expenseData = {
@@ -10,12 +12,22 @@ const NewExpense = (props) => {
       id: Math.random().toString()
     }
     //console.log(expenseData)
-    props.onGetExpenseData(expenseData)
+    props.onGetExpenseData(expenseData);
+    setIsEditing(false);
+  }
+
+  const startEditinghandler = () => {
+    setIsEditing(true);
+  }
+
+  const stopEditinghandler = () => {
+    setIsEditing(false);
   }
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={onSaveExpenseData}/>
+     {!isEditing && <button type="submit" onClick={startEditinghandler}>Add New Expense</button> }
+     {isEditing &&  <ExpenseForm onSaveExpenseData={onSaveExpenseData} onCancel={stopEditinghandler}/>}
     </div>
   )
 }
